821f4a9f69b90db2255ad5843deb3e5df8d5f036
johan <stjernholm@msn.com>
2020-02-04 00:12:31 +0800

    Feature: add models interfaces

---
+++ src/models/Topic.ts
@@ -0,0 +1,7 @@
+export interface Topic {
+  userId: string
+  topicId: string
+  createdAt: string
+  description: string
+  attachmentUrl?: string
+}
---
+++ src/models/TopicUpdate.ts
@@ -0,0 +1,3 @@
+export interface TopicUpdate {
+  description: string
+}
