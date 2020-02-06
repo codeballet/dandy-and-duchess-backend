9fc2b162309923472f8c4d13c9b3550b44cd9fd0
johan <stjernholm@msn.com>
2020-02-06 17:00:36 +0800

    Feature: add S3 image delete

--- src/lambda/http/deleteTopic.ts
+++ src/lambda/http/deleteTopic.ts
@@ -1,11 +1,13 @@
 import 'source-map-support/register'
-
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import { deleteTopic } from '../../businessLogic/topics'
 import { getUserId } from '../utils'
 import { createLogger } from '../../utils/logger'
 
 const logger = createLogger('deleteTopic')
+const s3 = new AWS.S3()
+const bucketName = process.env.IMAGES_S3_BUCKET
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
@@ -17,6 +19,25 @@ export const handler: APIGatewayProxyHandler = async (
   const userId = getUserId(event)
 
   const deletedTopic = await deleteTopic(topicId, userId)
+
+  // Remove the image from S3
+  const params = {
+    Bucket: bucketName,
+    Key: topicId
+  }
+
+  try {
+    await s3.headObject(params).promise()
+    logger.info('File found in S3')
+    try {
+      await s3.deleteObject(params).promise()
+      logger.info('File deleted successfully')
+    } catch (e) {
+      logger.info(`ERROR in deleting file: ${e}`)
+    }
+  } catch (e) {
+    logger.info(`File not found ERROR: ${e}`)
+  }
 
   return {
     statusCode: 201,
